65a1a3b12c3cb5d39e49afe0e3a3406bfb214901
ahmedalgohary1170 <ahmedalgohary1170@gmail.com>
2024-05-18 20:21:07 +0300

    design inventory app models

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Book , Author , Category
 # Register your models here.
+
+
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Category)
+
+
+
+
+
+
+
+
+
+
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,25 @@
+
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL,null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+
+    def _str_(self):
+        return self.title
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    biography = models.TextField(blank=True)
+
+    def _str_(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def _str_(self):
+        return self.name
